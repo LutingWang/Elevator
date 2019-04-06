98b60cf9904001ceed5e64f63f0373d8fcf810c4
LutingWang <2457348692@qq.com>
2019-04-06 12:13:13 +0800

    complementary files

---
+++ test/datacheck.bat
@@ -0,0 +1,4 @@
+set CURRENT_DATE_TIME=%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%    
+echo %CURRENT_DATE_TIME% >> log.txt
+datacheck.exe -i datacheck_in.txt >> log.txt
+echo. >> log.txt
