e1025f77032ca76fa1509fa5f1715cecd4481ed0
LutingWang <2457348692@qq.com>
2019-04-16 18:36:43 +0800

    final

--- src/model/Elevator.java
+++ src/model/Elevator.java
@@ -115,7 +115,7 @@ public class Elevator implements AutoStart {
                 attrLock.unlock();
             }
         }
-    
+        
         Dir direction() {
             if (stop(floor)) {
                 return Dir.STOP;
@@ -159,7 +159,7 @@ public class Elevator implements AutoStart {
                 in.getLock().readLock().unlock();
             }
         }
-    
+        
         Boolean stop() {
             return stop(floorCache);
         }
@@ -181,23 +181,17 @@ public class Elevator implements AutoStart {
                 Dir temp = dirCache.cache();
                 attrLock.unlock();
                 if (temp == Dir.STOP) {
-                    statusLock.readLock().lock();
-                    if (status == Status.OPEN) {
-                        doorClosed.signalAll();
-                    } else {
+                    statusLock.writeLock().lock();
+                    if (status == Status.NULL) {
+                        status = Status.RUNNING; // any status but null
                         running.signalAll();
                         noDirection.signalAll();
+                    } else if (status == Status.RUNNING) {
+                        running.signalAll();
+                    } else { // status == Status.OPEN
+                        doorClosed.signalAll();
                     }
-                    //if (status == Status.NULL) {
-                    //    status = Status.RUNNING; // any status but null
-                    //    running.signalAll();
-                    //    noDirection.signalAll();
-                    //} else if (status == Status.RUNNING) {
-                    //    running.signalAll();
-                    //} else { // status == Status.OPEN
-                    //    doorClosed.signalAll();
-                    //}
-                    statusLock.readLock().unlock();
+                    statusLock.writeLock().unlock();
                 } else if (temp != Dir.NULL) {
                     if (temp == Dir.UP) {
                         floorCache = floor + 1;
@@ -309,30 +303,17 @@ public class Elevator implements AutoStart {
     public void run() {
         in.start();
         manager.start();
-        boolean returnFromIdle = false;
         try {
-            //statusLock.writeLock().lock();
-            //status = Status.NULL;
-            //running.await(speed, TimeUnit.MILLISECONDS);
-            //Tools.threadMonitor();
-            //if (status == Status.NULL) {
-            //    noDirection.await();
-            //    Tools.threadMonitor();
-            //}
-            //status = Status.NULL;
-            //statusLock.writeLock().unlock();
             while (isAlive()) {
                 statusLock.writeLock().lock();
-                if (!returnFromIdle) {
-                    running.await(speed, TimeUnit.MILLISECONDS);
-                }
-                returnFromIdle = false;
                 Dir temp = manager.getDir();
                 if (temp == Dir.NULL) {
                     status = Status.NULL;
-                    noDirection.await();
-                    Tools.threadMonitor();
-                    returnFromIdle = true;
+                    running.await(speed, TimeUnit.MILLISECONDS);
+                    if (status == Status.NULL) {
+                        noDirection.await();
+                    }
+                    status = Status.NULL;
                 } else {
                     if (temp == Dir.UP) {
                         floor++;
@@ -353,6 +334,7 @@ public class Elevator implements AutoStart {
                         status = Status.RUNNING;
                     }
                     signalAll("notNull");
+                    running.await(speed, TimeUnit.MILLISECONDS);
                 }
             }
         } catch (InterruptedException e) {
